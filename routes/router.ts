a104bba11adce02eab4834ae4099775913443f16
Ismail <ibaslam20@gmail.com>
2025-10-09 12:20:54 +0200

    Structure projet + docker + env sample + db

---
+++ routes/router.ts
@@ -0,0 +1,9 @@
+import {Express} from "express";
+
+const express = require("express");
+
+const router = express.Router();
+
+module.exports = router;
+
+
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+// const expres = require("express")
+//
+// const app = expres()
+//
+// app.use(expres.json)
+//
+// app.listen(8080, ()=> {console.log("test réussie")})
+//
+
+
+import 'dotenv/config';
+console.log('DB URL:', process.env.DATABASE_URL);
